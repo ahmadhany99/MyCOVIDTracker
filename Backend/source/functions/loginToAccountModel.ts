a292477fba0ae2c8a9330aa9ae2918356c4114c8
mint47 <dor012299@gmail.com>
2022-03-17 11:19:03 -0400

    refactored login to use loginDTO instead of accountModel

---
+++ Backend/source/functions/loginToAccountModel.ts
@@ -0,0 +1,17 @@
+import { accountModel } from "../models/account";
+import { loginDTO } from "../models/loginDTO";
+
+export function loginDTOToAccountModel (accDTO: loginDTO) {
+    const accModel: accountModel = {
+        accountId: NaN,
+        username: accDTO.username,
+        password: accDTO.password,
+        firstname: '',
+        lastname: '',
+        cellPhone: '',
+        email: '',
+        typeId: NaN,
+        languageId: NaN
+    }
+    return accModel;
+};
