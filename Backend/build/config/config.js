6caae43baea13a95755e6d6cac28f6fcc764e6bb
mikeymousey <mikelaplaine@gmail.com>
2022-03-20 19:45:21 -0400

    changed

--- Backend/build/config/config.js
+++ Backend/build/config/config.js
@@ -4,14 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-
 dotenv_1.default.config();
-
 const MYSQL_USER = process.env.MYSQL_HOST || 'root';
-const MYSQL_PASSWORD = process.env.MYSQL_HOST || 'soen390';
+console.info('MYSQL HOST****: ' + process.env.MYSQL_HOST);
+const MYSQL_PASSWORD = process.env.MYSQL_DATABASE || 'soen390';
 const MYSQL_HOST = process.env.MYSQL_HOST || '35.223.223.15';
 const MYSQL_DATABASE = process.env.MYSQL_HOST || 'soen390';
-
 // Defining MYSQL Object
 const MYSQL = {
     host: MYSQL_HOST,
@@ -21,7 +19,6 @@ const MYSQL = {
 };
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.PORT || 1337;
-
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || 3600; // expire time in seconds
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'coolIsuer'; //  organization name
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'superencryptedsecret'; //  encrypting jwt
