1e1a56a56fd5ae54836e76cca36785e833e475d5
ahmadhany99 <ahmadhany1999@hotmail.com>
2022-03-21 18:26:54 -0400

    Updated the account.ts

--- Backend/source/controllers/account.ts
+++ Backend/source/controllers/account.ts
@@ -2,15 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import logging from '../config/logging';
 import * as accountService from '../services/account';
 import { accountModel } from '../models/account';
-import { logintDTO } from '../models/loginDTO';
+import { loginDTO } from '../models/loginDTO';
 import signJWT from '../functions/signJWT';
-
-
 
 const NAMESPACE = 'account/controller';
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
-    logging.info(NAMESPACE, "Create account");
+    logging.info(NAMESPACE, 'Create account');
 
     //  Data Transfer Object (DTO)
     const accountDTO: accountModel = req.body;
@@ -22,18 +20,15 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
         // Return a response to client.
         return res.status(200).json({
             status: 200,
-            message: "Account Created."
-        })
-
+            message: 'Account Created.'
+        });
     } catch (e) {
         return res.status(500).json(e);
     }
-
-}
+};
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Login to account.');
-
 
     //  Data Transfer Object (DTO)
     const accountDTO: loginDTO = req.body;
@@ -43,8 +38,8 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
         if (!accountDTO.username || !accountDTO.password) {
             return res.status(400).json({
                 status: 400,
-                message: "Missing username or password"
-            })
+                message: 'Missing username or password'
+            });
         }
 
         //  Call to service layer
@@ -52,67 +47,55 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
 
         // Return a response to client.
         return res.json(result);
-
     } catch (e) {
         return res.status(500).json(e);
     }
-
-}
+};
 
 const getAccount = async (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Retrieving Account from Database');
-
 
     //  Data Transfer Object (DTO)
     const accountDTO: accountModel = req.body;
 
     try {
-
         //  Call to service layer
         const result = await accountService.getAccount(accountDTO);
 
         // Return a response to client.
         return res.json(result);
-
     } catch (e) {
         return res.status(500).json(e);
     }
-
-}
+};
 
 const getAllDoctors = async (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Retrieving Doctors from Database');
 
     try {
-
         //  Call to service layer
         const result = await accountService.getAllDoctors();
 
         // Return a response to client.
         return res.json(result);
-
     } catch (e) {
         return res.status(500).json(e);
     }
-
-}
+};
 
 const getAllPatients = async (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Retrieving Patients from Database');
 
     try {
-
         //  Call to service layer
         const result = await accountService.getAllPatients();
 
         // Return a response to client.
         return res.json(result);
-
     } catch (e) {
         return res.status(500).json(e);
     }
-
-}
+};
 
 const deleteAccount = async (req: Request, res: Response, next: NextFunction) => {
     logging.info(NAMESPACE, 'Deleting Account');
@@ -123,12 +106,10 @@ const deleteAccount = async (req: Request, res: Response, next: NextFunction) =>
         const result = await accountService.deleteAccount(accountDTO);
 
         return res.json(result);
-    }
-    catch (err) {
+    } catch (err) {
         return res.status(500).json(err);
     }
-
-}
+};
 
 export default {
     register,
