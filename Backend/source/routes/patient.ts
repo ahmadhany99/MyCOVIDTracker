a8fbc8c72ba2d64d0a12ac375628e26e8cd591ba
ahmadhany99 <ahmadhany1999@hotmail.com>
2022-03-28 19:01:49 -0400

    added a post route for getpatient

--- Backend/source/routes/patient.ts
+++ Backend/source/routes/patient.ts
@@ -4,6 +4,7 @@ import patientController from '../controllers/patient';
 const router = express.Router();
 
 router.get('/patient/getPatient', patientController.getPatient);
+router.post('/patient/getPatient', patientController.getPatient);
 router.get('/patient/countAllPatients', patientController.countAllPatients);
 
 export = router;
